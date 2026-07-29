64c4cfcf54fc6508c59cc2fa75fb18b487967ae8
iyiznil <darren.lzy99@gmail.com>
2026-07-29 13:16:56 +0300

    Lesson 3 Completed

--- lesson3/nodes/pure_pursuit_follower.py
+++ lesson3/nodes/pure_pursuit_follower.py
@@ -16,9 +16,8 @@ class PurePursuitFollower:
     def __init__(self):
 
         # Parameters
-        # TODO 4: Read in parameter values:
-        #         self.lookahead_distance = rospy.get_param("~lookahead_distance")
-        #         self.wheel_base = rospy.get_param("/vehicle/wheel_base")
+        self.lookahead_distance = rospy.get_param("~lookahead_distance")
+        self.wheel_base = rospy.get_param("/vehicle/wheel_base")
 
         # Internal variables
         self.lock = Lock()
@@ -26,8 +25,8 @@ class PurePursuitFollower:
         self.distance_to_velocity_interpolator = None
 
         # Publishers
-        # TODO 2: Create a publisher for '/control/vehicle_cmd' topic with VehicleCommand message type.
-        #         self.vehicle_cmd_pub = rospy.Publisher(...)
+        self.vehicle_cmd_pub = rospy.Publisher('/control/vehicle_cmd', VehicleCommand, queue_size=1)
+
 
         # Subscribers
         rospy.Subscriber('path', Path, self.path_callback, queue_size=1)
@@ -38,19 +37,15 @@ class PurePursuitFollower:
             path_linestring = None
             distance_to_velocity_interpolator = None
         else:
-            # TODO 3: Convert waypoints to a shapely LineString and prepare it for spatial queries.
-            #         - Create LineString from waypoint (x, y) coordinates
-            #         - Use prepare() to create a spatial index for efficient queries
+            path_linestring = LineString([(w.position.x, w.position.y) for w in msg.waypoints])
+            prepare(path_linestring)
 
-            path_linestring = None
 
-            # TODO 5: Create a distance-to-velocity interpolator for the path.
-            #         - Collect waypoint (x, y) coordinates into a numpy array
-            #         - Calculate cumulative distances between waypoints
-            #         - Extract velocity values from waypoints
-            #         - Create an interp1d interpolator (bounds_error=False, fill_value=0.0)
-
-            distance_to_velocity_interpolator = None
+            waypoints_xy = np.array([(w.position.x, w.position.y) for w in msg.waypoints])
+            distances = np.cumsum(np.sqrt(np.sum(np.diff(waypoints_xy, axis=0)**2, axis=1)))
+            distances = np.insert(distances, 0, 0)
+            velocities = np.array([w.speed for w in msg.waypoints])
+            distance_to_velocity_interpolator = interp1d(distances, velocities, kind='linear', bounds_error=False, fill_value=0.0)
 
         with self.lock:
             self.path_linestring = path_linestring
@@ -58,27 +53,25 @@ class PurePursuitFollower:
 
     def current_pose_callback(self, msg):
         if self.path_linestring is None:
-            # TODO 1: Print out the current pose coordinates to verify data flow.
-            #         print(msg.pose.position.x, msg.pose.position.y)
-
             steering_angle = 0.0
             linear_velocity = 0.0
             linear_acceleration = -3.0
+
+            self.distance_to_velocity_interpolator is None
+
         else:
-            # TODO 3: Calculate the ego vehicle's distance from the path start.
-            #         - Convert ego position to a shapely Point
-            #         - Use self.path_linestring.project() to find the distance
-            #         - Remove the TODO 1 printout and print d_ego_from_path_start instead
+            current_pose = Point([msg.pose.position.x, msg.pose.position.y])
+            d_ego_from_path_start = self.path_linestring.project(current_pose)
+            print(d_ego_from_path_start)
 
-            # TODO 4: Calculate the steering angle using the Pure Pursuit formula.
-            #         - Get heading from msg.pose.orientation using euler_from_quaternion
-            #         - Calculate the lookahead point using self.path_linestring.interpolate()
-            #         - Calculate the lookahead heading using np.arctan2
-            #         - Calculate the actual lookahead distance using shapely distance()
-            #         - Apply the steering formula
+            _, _, heading = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+            lookahead_point = self.path_linestring.interpolate(d_ego_from_path_start + self.lookahead_distance)
+            lookahead_heading = np.arctan2(lookahead_point.y - msg.pose.position.y, lookahead_point.x - msg.pose.position.x)
+            ld = np.sqrt((lookahead_point.x - msg.pose.position.x) ** 2 + (lookahead_point.y - msg.pose.position.y) ** 2)
+            steering_angle = np.arctan2(2 * self.wheel_base * np.sin(lookahead_heading - heading) / ld, 1.0)
 
-            steering_angle = 0.0
-            linear_velocity = 0.0
+
+            linear_velocity = float(self.distance_to_velocity_interpolator(d_ego_from_path_start))
             linear_acceleration = 0.0
 
             # TODO 5: Use the distance-to-velocity interpolator to get the velocity
@@ -87,7 +80,15 @@ class PurePursuitFollower:
             #         Since the interpolator is now used here, add a check for
             #         self.distance_to_velocity_interpolator is None to the if statement above.
 
-        # TODO 2: Create and publish a VehicleCommand message with constant steering angle and velocity for testing.
+        vehicle_cmd = VehicleCommand()
+
+        vehicle_cmd.header.stamp = msg.header.stamp
+        vehicle_cmd.header.frame_id = "base_link"
+        vehicle_cmd.steering_angle = steering_angle
+        vehicle_cmd.speed = linear_velocity
+        vehicle_cmd.acceleration = linear_acceleration
+        print(vehicle_cmd.steering_angle, msg.pose.position.x, msg.pose.position.y)
+        self.vehicle_cmd_pub.publish(vehicle_cmd)
 
     def run(self):
         rospy.spin()
