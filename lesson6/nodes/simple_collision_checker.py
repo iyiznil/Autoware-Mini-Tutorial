5135952bbe6a969a277510fb55bfdc1c61cafa27
iyiznil <darren.lzy99@gmail.com>
2026-08-05 13:22:47 +0300

    Lesson 7 Completed

--- lesson6/nodes/simple_collision_checker.py
+++ lesson6/nodes/simple_collision_checker.py
@@ -6,7 +6,10 @@ import math
 import numpy as np
 import threading
 from ros_numpy import msgify
-from autoware_mini.msg import Path, DetectedObjectArray
+from autoware_mini.msg import Path, DetectedObjectArray, StopLineStatus, StopLineStatusArray
+import lanelet2
+from lanelet2.io import Origin, load
+from lanelet2.projection import UtmProjector
 from sensor_msgs.msg import PointCloud2
 
 # Collision point categories: 0 = none, 1 = goal, 2 = traffic light, 3 = static obstacle, 4 = moving obstacle
@@ -32,13 +35,26 @@ class SimpleCollisionChecker:
         self.stopped_speed_limit = rospy.get_param("stopped_speed_limit")
         self.braking_safety_distance_obstacle = rospy.get_param("~braking_safety_distance_obstacle")
         self.braking_safety_distance_goal = rospy.get_param("~braking_safety_distance_goal")
-        # TODO 8 (lesson 7): add braking_safety_distance_stopline parameter,
-        #                    load the lanelet2 map and extract the stop lines with traffic lights
+        self.braking_safety_distance_stopline = rospy.get_param("~braking_safety_distance_stopline")
+
+        lanelet2_map_path = rospy.get_param("~lanelet2_map_path")
+        coordinate_transformer = rospy.get_param("/localization/coordinate_transformer")
+        use_custom_origin = rospy.get_param("/localization/use_custom_origin")
+        utm_origin_lat = rospy.get_param("/localization/utm_origin_lat")
+        utm_origin_lon = rospy.get_param("/localization/utm_origin_lon")
+
+        if coordinate_transformer == "utm":
+            projector = UtmProjector(Origin(utm_origin_lat, utm_origin_lon), use_custom_origin, False)
+        else:
+            raise RuntimeError('Only "utm" is supported for lanelet2 map loading')
+        lanelet2_map = load(lanelet2_map_path, projector)
+
+        self.stop_lines = self.get_traffic_light_stop_lines(lanelet2_map)
 
         # Variables
         self.detected_objects = None
         self.goal_point = None
-        # TODO 8 (lesson 7): add stopline_statuses dict
+        self.stopline_statuses = {}
 
         # Lock for thread safety
         self.lock = threading.Lock()
@@ -50,7 +66,7 @@ class SimpleCollisionChecker:
         rospy.Subscriber('extracted_local_path', Path, self.path_callback, queue_size=1, tcp_nodelay=True)
         rospy.Subscriber('/detection/final_objects', DetectedObjectArray, self.detected_objects_callback, queue_size=1, buff_size=2**20, tcp_nodelay=True)
         rospy.Subscriber('global_path', Path, self.global_path_callback, queue_size=None, tcp_nodelay=True)
-        # TODO 8 (lesson 7): add traffic_light_status subscriber
+        rospy.Subscriber('/detection/traffic_light_status', StopLineStatusArray, self.traffic_light_status_callback, queue_size=1, tcp_nodelay=True)
 
         rospy.loginfo("%s - initialized", rospy.get_name())
 
@@ -121,7 +137,28 @@ class SimpleCollisionChecker:
                         np.inf, 1
               )], dtype=DTYPE))
 
-        # TODO 9 (lesson 7): add stop line collision points for red traffic lights
+        for stop_line_id, status in self.stopline_statuses.items():
+            if status != StopLineStatus.STATUS_STOP:
+                continue
+            stop_line = self.stop_lines.get(stop_line_id)
+            if stop_line is None or not local_path_buffer.intersects(stop_line):
+                continue
+
+            intersection = local_path_linestring.intersection(stop_line)
+            intersection_points = shapely.get_coordinates(intersection)[:, :2]
+
+            for x, y in intersection_points:
+                collision_points = np.append(collision_points, np.array([(
+                    x,
+                    y,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    self.braking_safety_distance_stopline,
+                    np.inf,
+                    2,
+                    )], dtype=DTYPE))
 
         # Publish the collision points (an empty array means no collision points on the path)
         if len(collision_points) > 0:
@@ -130,6 +167,19 @@ class SimpleCollisionChecker:
             collision_points_msg = PointCloud2()
         collision_points_msg.header = msg.header
         self.collision_points_pub.publish(collision_points_msg)
+
+    def traffic_light_status_callback(self, msg):
+        self.stopline_statuses = {status.stop_line_id: status.status for status in msg.statuses}
+
+    @staticmethod
+    def get_traffic_light_stop_lines(lanelet2_map):
+        stop_lines = {}
+        for reg_el in lanelet2_map.regulatoryElementLayer:
+            if reg_el.attributes["subtype"] == "traffic_light":
+                stop_line = reg_el.parameters["ref_line"][0]
+                stop_lines[stop_line.id] = shapely.LineString([(p.x, p.y) for p in stop_line])
+
+        return stop_lines
 
     def run(self):
         rospy.spin()
--- lesson7/nodes/yolo_traffic_light_detector.py
+++ lesson7/nodes/yolo_traffic_light_detector.py
@@ -124,9 +124,11 @@ class YoloTrafficLightDetector:
         # used in calculate_roi_coordinates to filter out only relevant traffic lights
         stop_line_ids_on_path = []
 
-        # TODO 1: If the local path has waypoints, create a shapely LineString from them
-        #         and collect the ids of the stop lines that intersect with it
-        #         into stop_line_ids_on_path.
+        if len(local_path_msg.waypoints) > 0:
+            local_path_line = shapely.LineString([(wp.position.x, wp.position.y) for wp in local_path_msg.waypoints])
+            for stop_line_id, stop_line in self.stop_lines.items():
+                if local_path_line.intersects(stop_line):
+                    stop_line_ids_on_path.append(stop_line_id)
 
         with self.lock:
             self.stop_line_ids_on_path = stop_line_ids_on_path
@@ -167,13 +169,20 @@ class YoloTrafficLightDetector:
 
         if stop_line_ids_on_path:
 
-            # TODO 2: Extract the transform between transform_to_frame and transform_from_frame
-            #         at image_time_stamp using self.tf_buffer, then calculate the map ROIs:
+            try:
+                transform = self.tf_buffer.lookup_transform(transform_to_frame, transform_from_frame, image_time_stamp)
+            except (tf2_ros.TransformException, rospy.ROSTimeMovedBackwardsException) as e:
+                rospy.logwarn("%s - %s", rospy.get_name(), e)
+                return
+            map_rois = self.calculate_roi_coordinates(stop_line_ids_on_path, transform)
 
             if map_rois:
-                # TODO 4: Run the YOLO model on the image to get yolo_rois, classes and scores,
-                #         then match them with map_rois:
-                pass
+                yolo_rois, classes, scores = self.yolo_model.predict(image)
+                # keep only the main traffic light classes, discard arrow-specific detections
+                mask = classes < 4
+                yolo_rois, classes, scores = yolo_rois[mask], classes[mask], scores[mask]
+                tfl_results, match_dict = self.match_map_and_yolo_rois(map_rois, yolo_rois, classes, scores)
+                tfl_status.statuses.extend(tfl_results)
 
         self.tfl_status_pub.publish(tfl_status)
 
@@ -190,10 +199,10 @@ class YoloTrafficLightDetector:
                 for x, y, z in traffic_light_coords:
                     point_map = Point(x=x, y=y, z=z)
 
-                    # TODO 3: Transform point_map to the camera frame (point_camera),
-                    #         project it to pixel coordinates (u, v) using the camera model
-                    #         and break out of the loop if the point is outside the image
-                    #         or behind the camera.
+                    point_camera = do_transform_point(PointStamped(point=point_map), transform).point
+                    u, v = self.camera_model.project3dToPixel((point_camera.x, point_camera.y, point_camera.z))
+                    if u < 0 or u >= self.camera_model.width or v < 0 or v >= self.camera_model.height or point_camera.z <= 0:
+                        break 
 
                     # convert the extent in meters to extent in pixels
                     extent_x_px = self.camera_model.fx() * self.roi_width_extent / point_camera.z
@@ -232,10 +241,11 @@ class YoloTrafficLightDetector:
         for stop_line_id, traffic_light_id, x1_map, x2_map, y1_map, y2_map in map_rois:
             matched_roi = None
 
-            # TODO 4: Find the YOLO detection that best matches this map ROI.
-            #         Loop over the YOLO detections, calculate the IOU between the map ROI
-            #         and the YOLO box, skip matches with IOU below self.iou_threshold and
-            #         keep the one with the highest IOU:
+            for idx, (cls, score, yolo_roi) in enumerate(zip(yolo_classes, yolo_scores, yolo_rois)):
+                iou_score = self.calculate_iou(np.array([[x1_map, y1_map, x2_map, y2_map]]), yolo_roi[np.newaxis, :])[0][0]
+                if iou_score >= self.iou_threshold:
+                    if matched_roi is None or iou_score > matched_roi[0]:
+                        matched_roi = [cls, score, yolo_roi, idx]
 
             tfl_result = StopLineStatus()
             tfl_result.traffic_light_id = traffic_light_id
@@ -247,9 +257,8 @@ class YoloTrafficLightDetector:
                 tfl_result.status_text = "missing"
                 match_dict[traffic_light_id] = None
             else:
-                # yolo ROI and map ROI were matched
-                # TODO 4: Fill in the status using CLASS_TO_TLRESULT
-                #         and the status_text using CLASS_TO_STRING.
+                tfl_result.status = CLASS_TO_TLRESULT[matched_roi[0]]
+                tfl_result.status_text = CLASS_TO_STRING[matched_roi[0]]
                 match_dict[traffic_light_id] = matched_roi
 
             tfl_results.append(tfl_result)
